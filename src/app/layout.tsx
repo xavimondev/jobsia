5dd0c53c9e03c2708de733fd283cffb304601b78
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-17 18:52:19 -0500

    fix lint issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,17 @@
-import './globals.css';
-import { Inter } from 'next/font/google';
+import './globals.css'
+import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Jobsia',
   description: 'Aplicación para la hackaton de infojobs'
-};
+}
 
-export default function RootLayout({
-  children
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
