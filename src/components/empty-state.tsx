e9149f717ea241d6ffceddb53c4d684464f6cf20
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-19 17:45:33 -0500

    add empty state

--- src/components/empty-state.tsx
+++ src/components/empty-state.tsx
@@ -1,4 +1,4 @@
-import { NoSearchResults } from './illustrations'
+import { NoSearchResults, WaitingSearch } from './illustrations'
 
 export function NotResultsFound() {
   return (
@@ -8,6 +8,15 @@ export function NotResultsFound() {
         <span className='font-bold'>Ningún resultado a la vista</span> Comprueba que esté bien
         escrito, usa otras palabras o revisa los filtros.
       </p>
+    </div>
+  )
+}
+
+export function NotJobSelected() {
+  return (
+    <div className='mt-2 hidden lg:flex justify-center items-center gap-2 flex-col text-center w-full h-full'>
+      <WaitingSearch className='h-[200px] w-[200px] sm:w-[400px] sm:h-[400px]' />
+      <p className='text-white text-2xl'>Selecciona una oferta de trabajo para ver el detalle</p>
     </div>
   )
 }
