da47d665a79804be111d414ae2a2da207009f2a1
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-19 22:50:17 -0500

    refactor by adding components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
-import { SearchForm } from '@/components/search-form'
 import { redirect } from 'next/navigation'
+import { SearchForm } from '@/components/search-form'
+import { ListSuggestions } from '@/components/list-suggestions'
 
 const handleSubmit = (query: string) => {
   redirect(`/jobs?q=${encodeURIComponent(query)}`)
@@ -20,22 +21,7 @@ export default function Home() {
           Disponible para desarrolladores, administradores de base de datos y diseñadores web
         </h2>
         <SearchForm onSubmit={handleSubmit} enteredQuery='' />
-        <div className='mx-auto max-w-6xl mt-4'>
-          <div className='flex gap-4 flex-wrap'>
-            <button className='border border-white border-opacity-10 text-sm sm:text-base w-full sm:w-auto hover:border-opacity-30 shadow-md rounded-lg transition duration-200 ease-in-out group hover:-translate-y-1 z-10 bg-[#13111a] text-[#bac8de] hover:text-white p-2'>
-              Desarrollador c# con ASP.NET con 2 años de experiencia y modalidad presencial
-            </button>
-            <button className='border border-white border-opacity-10 text-sm sm:text-base w-full sm:w-auto hover:border-opacity-30 shadow-md rounded-lg transition duration-200 ease-in-out group hover:-translate-y-1 z-10 bg-[#13111a] text-[#bac8de] hover:text-white p-2'>
-              Desarrollador Java con salario de 30.000 en Barcelona
-            </button>
-            <button className='border border-white border-opacity-10 text-sm sm:text-base w-full sm:w-auto hover:border-opacity-30 shadow-md rounded-lg transition duration-200 ease-in-out group hover:-translate-y-1 z-10 bg-[#13111a] text-[#bac8de] hover:text-white p-2'>
-              Programador a Java en Sevilla con contrato indefinido y modalidad teletrabajo
-            </button>
-            <button className='border border-white border-opacity-10 text-sm sm:text-base w-full sm:w-auto hover:border-opacity-30 shadow-md rounded-lg transition duration-200 ease-in-out group hover:-translate-y-1 z-10 bg-[#13111a] text-[#bac8de] hover:text-white p-2'>
-              Administrador de sistemas Linux en Madrid con contrato indefinido
-            </button>
-          </div>
-        </div>
+        <ListSuggestions />
       </section>
     </main>
   )
