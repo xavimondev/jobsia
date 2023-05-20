85d31321e2e41777a62c1c0d0c78cc50249b6914
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-20 11:59:21 -0500

    refactor icons

--- src/components/list-offers.tsx
+++ src/components/list-offers.tsx
@@ -1,5 +1,5 @@
 import { JobOffer } from '@/types'
-import { YoungIc } from './icons'
+import { LocationIc, MoneyIc, YoungIc } from './icons'
 
 type ListOffersProps = {
   jobDetailsFn: (offerId: string) => Promise<void>
@@ -20,34 +20,18 @@ export function ListOffers({ jobDetailsFn, offers }: ListOffersProps) {
               <h3 className='font-bold text-white text-lg line-clamp-1'>{offer.title}</h3>
               <h4 className='font-semibold text-white text-lg'>{offer.company.name}</h4>
               <span className='flex items-center text-[#f77e80] gap-1 text-sm'>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  width='18'
-                  height='18'
-                  fill='currentColor'
-                  viewBox='0 0 256 256'
-                >
-                  <path d='M128,16a88.1,88.1,0,0,0-88,88c0,75.3,80,132.17,83.41,134.55a8,8,0,0,0,9.18,0C136,236.17,216,179.3,216,104A88.1,88.1,0,0,0,128,16Zm0,56a32,32,0,1,1-32,32A32,32,0,0,1,128,72Z'></path>
-                </svg>
+                <LocationIc className='h-4 w-4' />
                 {offer.location.country}, {offer.location.province}
               </span>
               <p className='text-gray-400 text-sm line-clamp-2'>{offer.minRequirements}</p>
             </div>
             <div className='flex flex-row items-center gap-2 justify-between'>
               <span className='flex items-center text-blue-300 gap-1 text-sm'>
-                <YoungIc className='h-5 w-5' />
+                <YoungIc className='h-4 w-4' />
                 {offer.experienceMin}
               </span>
               <span className='flex items-center text-green-400 gap-1 text-sm'>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  width='18'
-                  height='18'
-                  fill='currentColor'
-                  viewBox='0 0 256 256'
-                >
-                  <path d='M168,128a40,40,0,1,1-40-40A40,40,0,0,1,168,128Zm80-64V192a8,8,0,0,1-8,8H16a8,8,0,0,1-8-8V64a8,8,0,0,1,8-8H240A8,8,0,0,1,248,64Zm-16,46.35A56.78,56.78,0,0,1,193.65,72H62.35A56.78,56.78,0,0,1,24,110.35v35.3A56.78,56.78,0,0,1,62.35,184h131.3A56.78,56.78,0,0,1,232,145.65Z'></path>
-                </svg>
+                <MoneyIc className='h-4 w-4' />
                 {offer.salaryDesc}
               </span>
             </div>
