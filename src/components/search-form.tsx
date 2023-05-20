f803c9f820824796040a0f3a3cc47fadcc69fbf9
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-19 20:09:48 -0500

    update handle submit logic

--- src/components/search-form.tsx
+++ src/components/search-form.tsx
@@ -10,18 +10,15 @@ type SearchFormProps = {
 export function SearchForm({ enteredQuery, onSubmit }: SearchFormProps) {
   const [searchValue, setSearchValue] = useState(enteredQuery)
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+  const handleSubmit = (formData: FormData) => {
+    const searchValue = formData.get('search') as string
     const query = searchValue.trim()
     if (query.length === 0) return
     onSubmit(query)
   }
 
   return (
-    <form
-      className='flex flex-col gap-5 md:flex-row md:items-center w-full'
-      onSubmit={handleSubmit}
-    >
+    <form className='flex flex-col gap-5 md:flex-row md:items-center w-full' action={handleSubmit}>
       <div className='relative w-full'>
         <div className='flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none'>
           <SearchIc className='w-5 h-5 text-gray-500' />
