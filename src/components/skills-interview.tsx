1164760d72e32ff15e1d62c3e1a16526c50d95e7
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-23 15:17:50 -0500

    add component to handle interview

--- src/components/skills-interview.tsx
+++ src/components/skills-interview.tsx
@@ -1,30 +1,88 @@
 'use client'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { Interview } from '@/types'
+import { startInterview } from '@/services/interview'
 import { Speech } from './speech'
 // Testing: 307777a5f54cd09d3b6440ddf7d98a
-export function SkillsInterview() {
+
+type SkillsInterviewProps = {
+  candidate: string
+  position: string
+  company: string
+  category: string
+  location: string
+}
+
+export function SkillsInterview({ candidate, position, company, location }: SkillsInterviewProps) {
+  // const initialMessage = `¡Bienvenido, ${candidate}! En ${company} estamos buscando un ${position} para nuestro equipo. La oferta es para la ciudad de ${location}.
+  // A lo largo de la entrevista, exploraremos diferentes aspectos para evaluar tu idoneidad para el puesto.`
+  const [currentQuestion, setCurrentQuestion] = useState<string>(
+    '¿Qué nos podrías contar sobre ti?'
+  )
+  const [userAnswer, setUserAnswer] = useState('')
+  const [interview, setInterview] = useState<Interview[]>([])
+
   useEffect(() => {
-    console.log('Here it should show information from chatgpt')
-    const utterance = new SpeechSynthesisUtterance(
-      `Entendido. Para realizar una entrevista de 10 minutos, podemos establecer un límite de tiempo para cada pregunta y respuesta. Asegurémonos de mantener un ritmo constante para cubrir la mayor cantidad de preguntas posibles en ese tiempo. Durante la simulación. ¿Estás listo para comenzar la entrevista de trabajo de 10 minutos?`
-    )
-    utterance.lang = 'es-US'
-    utterance.rate = 0.7
-    speechSynthesis.speak(utterance)
-    //const voices = speechSynthesis.getVoices()
-    //console.log(voices)
-    //utterance.voice = voices.at(125)!
+    // https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesisUtterance
+    // const utterance = new SpeechSynthesisUtterance(initialMessage)
+    // utterance.lang = 'es-US'
+    // utterance.rate = 0.8
+    // speechSynthesis.speak(utterance)
   }, [])
+
+  async function startChatting() {
+    const questions = interview.map((intw) => intw.question)
+    const payload = {
+      candidate,
+      position,
+      company,
+      currentQuestion,
+      userAnswer,
+      totalQuestions: interview.length,
+      questions
+    }
+    //console.log(payload)
+    const data = await startInterview(payload)
+    console.log(data)
+    if (data.isEndInterview) {
+      setCurrentQuestion(data.message)
+      return
+    }
+    /*
+      Once I have score, feedback and answer, lets add the part of the conversation into the interview array.
+      Then, update current question
+    */
+    const { feedback, score, next_question } = data
+    // Update interview conversation
+    const messageInterview: Interview = {
+      question: currentQuestion,
+      answer: userAnswer,
+      score,
+      feedback
+    }
+    setInterview((prevMessages) => [...prevMessages, messageInterview])
+    // Update current question
+    setCurrentQuestion(next_question)
+  }
 
   return (
     <div className='flex flex-row gap-4 mt-8'>
       <div className='text-yellow-300 text-xl xl:text-3xl w-1/2'>
-        <p className='italic'>
-          Como te identifica a ti mismo dentro de unos años y como crees que debo decir un texto
-          largo que no debe exceder los 100 tokens
-        </p>
+        <p className='italic text-center'>{currentQuestion}</p>
       </div>
       <Speech />
+      <div>
+        <textarea
+          value={userAnswer}
+          onChange={(e) => setUserAnswer(e.target.value)}
+          name='input'
+          cols={20}
+          rows={10}
+        ></textarea>
+        <button className='p-2 bg-teal-600 text-white' onClick={startChatting}>
+          Enviar texto
+        </button>
+      </div>
     </div>
   )
 }
