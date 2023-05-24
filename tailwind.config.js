ef67c6426cee836ee6165da6073ad9c823ba72a2
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-24 16:54:13 -0500

    add accordion component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 import { withAnimations } from 'animated-tailwindcss'
+import { mauve, violet } from '@radix-ui/colors'
 
 module.exports = withAnimations({
   content: [
@@ -12,6 +13,24 @@ module.exports = withAnimations({
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      },
+      colors: {
+        ...mauve,
+        ...violet,
+      },
+      keyframes: {
+        slideDown: {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        slideUp: {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 },
+        }
+      },
+      animation: {
+        slideDown: 'slideDown 300ms cubic-bezier(0.87, 0, 0.13, 1)',
+        slideUp: 'slideUp 300ms cubic-bezier(0.87, 0, 0.13, 1)',
       }
     }
   },
