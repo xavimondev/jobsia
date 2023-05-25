9c3c3898654d1e49f1d43c38767e8206e40d5cde
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-25 13:05:48 -0500

    add util to display bye message

---
+++ src/utils/getByeMessage.ts
@@ -0,0 +1,6 @@
+export const getByeMessage = (position: string, company: string, candidate: string) => {
+  return `${candidate} quiero agradecerte sinceramente por dedicar tu tiempo al proceso de entrevista para el puesto de ${position} en ${company}.
+  Tu participación fue valiosa y apreciada.
+  Tu entusiasmo y habilidades son impresionantes, y estoy seguro de que encontrarás una gran oportunidad para crecer. 
+  A continuación verás los resultados de la entrevista`
+}
