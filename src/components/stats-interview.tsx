685c66ac07a99623c850818d651b3a737c08a811
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-25 15:14:12 -0500

    update ui with new constants

--- src/components/stats-interview.tsx
+++ src/components/stats-interview.tsx
@@ -1,12 +1,7 @@
 'use client'
 import { useMemo } from 'react'
 import { useStore } from '@/store'
-import { INTERVIEW_TOTAL_QUESTIONS, INTERVIEW_MAX_SCORE } from '@/utils/constants'
-
-// Five is the maximum score
-const TOTAL_SCORE = INTERVIEW_TOTAL_QUESTIONS * INTERVIEW_MAX_SCORE
-// 0.6 can change, it's up to the user
-const MAX_SCORE = TOTAL_SCORE * 0.6
+import { INTERVIEW_MAX_SCORE, SCORE_TO_ACCOMPLISH } from '@/utils/constants'
 
 export function StatsInterview() {
   const getTotalScore = useStore((state) => state.getTotalScore)
@@ -18,10 +13,10 @@ export function StatsInterview() {
   }, [interview])
 
   return (
-    <div className='bg-[#0d0d15] absolute bottom-0 py-3.5 w-full flex justify-center font-bold text-xl gap-6'>
+    <div className='bg-[#0d0d15] absolute bottom-0 py-3.5 w-full flex justify-center font-semibold text-lg xl:text-xl gap-6'>
       <h4 className='text-green-400'>PUNTAJE ACTUAL: {totalScore}</h4>
-      <h4 className='text-blue-400'>PUNTAJE NECESARIO: {MAX_SCORE}</h4>
-      <h4 className='text-red-400'>PUNTAJE MÁXIMO: {TOTAL_SCORE}</h4>
+      <h4 className='text-blue-400'>PUNTAJE NECESARIO: {SCORE_TO_ACCOMPLISH}</h4>
+      <h4 className='text-red-400'>PUNTAJE MÁXIMO: {INTERVIEW_MAX_SCORE}</h4>
     </div>
   )
 }
