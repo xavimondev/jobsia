03cfb7e524addc2804497997838f8395d86bbb27
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-25 17:32:20 -0500

    refactor component

--- src/components/results-interview.tsx
+++ src/components/results-interview.tsx
@@ -1,33 +1,17 @@
-import { INTERVIEW_MAX_SCORE, INTERVIEW_TOTAL_QUESTIONS } from '@/utils/constants'
-import { EvaluationExcellent } from '@/components/illustrations'
+import { EvaluationExcellent } from './illustrations'
+import { FeedbackInterview } from './feedback-interview'
+import { ScoreInterview } from './score-interview'
 
 export function ResultsInterview() {
   return (
-    <div className='flex flex-col items-center gap-4 mx-auto max-w-4xl animate-bounce animate-delay-200'>
-      <EvaluationExcellent className='w-[500px] h-[500px] xl:w-[600px] xl:h-[600px]' />
-      <p className='text-white text-2xl xl:text-4xl font-semibold text-center'>
-        <span className='text-transparent bg-clip-text bg-gradient-to-r from-[#b6eead] to-[#59bc6b]'>
-          Felicidades
-        </span>
-        , has obtenido{' '}
-        <span className='text-transparent bg-clip-text bg-gradient-to-r from-[#b6eead] to-[#59bc6b]'>
-          30
-        </span>{' '}
-        puntos de{' '}
-        <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-blue-600'>
-          {INTERVIEW_TOTAL_QUESTIONS * INTERVIEW_MAX_SCORE}
-        </span>
-      </p>
-      <p className='text-white text-2xl xl:text-4xl font-semibold'>
-        Con esto estarías apto para la siguiente fase.
-      </p>
-      <p className='text-white text-2xl xl:text-4xl font-semibold animate-slideInUp'>
-        A continuación, verás el{' '}
-        <span className='text-transparent bg-clip-text bg-gradient-to-r from-[#b6eead] to-[#59bc6b]'>
-          resumen de tu entrevista, puntaje y feedback
-        </span>{' '}
-        realizado por el asistente.
-      </p>
-    </div>
+    <section className='p-6 w-full h-screen grid place-items-center animate-fadeIn'>
+      <div className='flex items-center'>
+        <EvaluationExcellent className='w-[400px] h-[400px] xl:w-[600px] xl:h-[600px]' />
+        <div className='flex flex-col items-center gap-4'>
+          <ScoreInterview />
+          <FeedbackInterview />
+        </div>
+      </div>
+    </section>
   )
 }
