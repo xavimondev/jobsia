d61c658f970101bcc7fe7d8d63dda7a278e89b8f
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-25 17:31:30 -0500

    update feedback ui

--- src/components/feedback-interview.tsx
+++ src/components/feedback-interview.tsx
@@ -3,15 +3,15 @@ import AccordionUi from '@/components/ui/accordion'
 
 export function FeedbackInterview() {
   return (
-    <div className='flex flex-col items-center w-full gap-7 mx-auto max-w-3xl'>
-      <div className='flex flex-col gap-4'>
-        <h2 className='font-semibold text-blue-400 text-3xl xl:text-5xl'>Feedback</h2>
-        <p className='text-white'>
+    <div className='flex flex-col w-full gap-7'>
+      <div className='flex flex-col text-white'>
+        {/* <h2 className='font-semibold text-blue-400 text-3xl xl:text-5xl'>Feedback</h2> */}
+        <p className=''>
           Puedes ver el puntaje que te dió el asistente a cada pregunta y el feedback para mejorar
           en la siguiente entrevista.
         </p>
-        <p className='text-white'>
-          Puedes revisar el resultado de otros visitando el{' '}
+        <p className=''>
+          Revisa el resultado de otros visitando el{' '}
           <Link href='/dashboard' className='text-blue-400 underline'>
             dashboard
           </Link>
