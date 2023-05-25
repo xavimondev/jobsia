8295a62baa0a6304cdec22762fcd085ef94b728a
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-25 17:49:13 -0500

    add score result is displayed dynamically

--- src/components/results-interview.tsx
+++ src/components/results-interview.tsx
@@ -1,12 +1,22 @@
-import { EvaluationExcellent } from './illustrations'
+import { SCORE_TO_ACCOMPLISH } from '@/utils/constants'
+import { useStore } from '@/store'
+import { Discarded, EvaluationExcellent } from './illustrations'
 import { FeedbackInterview } from './feedback-interview'
 import { ScoreInterview } from './score-interview'
 
 export function ResultsInterview() {
+  const getTotalScore = useStore((state) => state.getTotalScore)
+  const hasIdealScore = getTotalScore() === SCORE_TO_ACCOMPLISH
+
   return (
     <section className='p-6 w-full h-screen grid place-items-center animate-fadeIn'>
       <div className='flex items-center'>
-        <EvaluationExcellent className='w-[400px] h-[400px] xl:w-[600px] xl:h-[600px]' />
+        {hasIdealScore ? (
+          <EvaluationExcellent className='w-[400px] h-[400px] xl:w-[600px] xl:h-[600px]' />
+        ) : (
+          <Discarded className='w-[400px] h-[400px] xl:w-[600px] xl:h-[600px]' />
+        )}
+
         <div className='flex flex-col items-center gap-4'>
           <ScoreInterview />
           <FeedbackInterview />
--- src/components/score-interview.tsx
+++ src/components/score-interview.tsx
@@ -1,20 +1,18 @@
-import { INTERVIEW_MAX_SCORE } from '@/utils/constants'
+import { INTERVIEW_MAX_SCORE, SCORE_TO_ACCOMPLISH } from '@/utils/constants'
 import { useStore } from '@/store'
 
 export function ScoreInterview() {
   const getTotalScore = useStore((state) => state.getTotalScore)
+  const hasIdealScore = getTotalScore() === SCORE_TO_ACCOMPLISH
+  const gradientStyles = `text-transparent bg-clip-text bg-gradient-to-r ${
+    hasIdealScore ? 'from-[#b6eead] to-[#59bc6b]' : 'from-[#ee6b69] to-[#e15050]'
+  }`
 
   return (
     <div className='text-3xl xl:text-5xl'>
       <p className='text-white font-semibold'>
-        <span className='text-transparent bg-clip-text bg-gradient-to-r from-[#b6eead] to-[#59bc6b]'>
-          Felicidades
-        </span>
-        , has obtenido{' '}
-        <span className='text-transparent bg-clip-text bg-gradient-to-r from-[#b6eead] to-[#59bc6b]'>
-          {getTotalScore()}
-        </span>{' '}
-        puntos de{' '}
+        <span className={gradientStyles}>{hasIdealScore ? 'Felicidades' : 'Lo siento'}</span>, has
+        obtenido <span className={gradientStyles}>{getTotalScore()}</span> puntos de{' '}
         <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-blue-600'>
           {INTERVIEW_MAX_SCORE}.
         </span>
