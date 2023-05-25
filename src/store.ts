459566355daccd615552c6881c3898c1bcd2e122
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-25 11:17:06 -0500

    add state to store

--- src/store.ts
+++ src/store.ts
@@ -1,21 +1,26 @@
 import { create } from 'zustand'
-import { Interview } from './types'
+import { EndInterviewStatus, Interview } from './types'
 
 type State = {
   interview: Interview[]
+  endInterviewStatus: EndInterviewStatus
 }
 
 type Action = {
   updateInterview: (interview: Interview) => void
   getTotalScore: () => number
+  setEndInterviewStatus: (endInterviewStatus: EndInterviewStatus) => void
 }
 
 export const useStore = create<State & Action>((set, get) => ({
   interview: [],
+  endInterviewStatus: { isInterviewSaved: false, isLastSpeech: false },
   updateInterview: (interviewData: Interview) =>
     set((prev) => ({ interview: [...prev.interview, interviewData] })),
   getTotalScore: () => {
     const interview = get().interview
     return interview.reduce((acc, curr) => acc + curr.score, 0)
-  }
+  },
+  setEndInterviewStatus: (endInterviewStatus: EndInterviewStatus) =>
+    set((prevState) => ({ ...prevState, ...endInterviewStatus }))
 }))
