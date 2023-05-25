8d40be54c0990142563914822f2c8e298525b3dc
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-25 10:37:40 -0500

    add service to save interview

--- src/services/interview.ts
+++ src/services/interview.ts
@@ -1,5 +1,22 @@
+import { Candidate, Interview } from '@/types'
+
 export const startInterview = async (payload: any) => {
   const response = await fetch('/api/interview', {
+    method: 'POST',
+    body: JSON.stringify(payload),
+    headers: {
+      'Content-type': 'application/json'
+    }
+  })
+  const data = await response.json()
+  return data
+}
+
+export const saveInterview = async (payload: {
+  candidate: Candidate
+  interviewDetails: Interview[]
+}) => {
+  const response = await fetch('/api/end-interview', {
     method: 'POST',
     body: JSON.stringify(payload),
     headers: {
