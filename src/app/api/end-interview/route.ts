ebbee7b8829ad68836a6afe2e669f63184946999
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-25 10:36:08 -0500

    add endpoint to save interview

---
+++ src/app/api/end-interview/route.ts
@@ -0,0 +1,74 @@
+import { cookies } from 'next/headers'
+import { NextResponse } from 'next/server'
+import { createClient } from '@supabase/supabase-js'
+import { Interview } from '@/types'
+
+const supabaseUrl = process.env.SUPABASE_URL as string
+const supabaseKey = process.env.SUPABASE_KEY as string
+
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json()
+    const { candidate, interviewDetails } = body
+    const { id, photo, name, surname1, surname2, city } = candidate
+
+    //Saving candidate data
+    let candidateId = id
+    // Checking if candidate exists
+    const { count } = await supabase
+      .from('candidates')
+      .select('*', { count: 'exact', head: true })
+      .eq('id', id)
+
+    if (count === 0) {
+      const { data } = await supabase
+        .from('candidates')
+        .insert({
+          id,
+          name,
+          surname: `${surname1} ${surname2}`.trim(),
+          photo,
+          city
+        })
+        .select('id')
+
+      candidateId = data![0].id
+    }
+    // Saving interview
+    // Getting offerId from cookies
+    const cookieStore = cookies()
+    const offerId = cookieStore.get('jobsia.offer-id')?.value
+    const { data } = await supabase
+      .from('interviews')
+      .insert({
+        offerId,
+        candidateId
+      })
+      .select('id')
+    // Saving interview details
+    // Getting interviewId from interviews table
+    const interviewId = data![0].id
+    const interviewDetailsData = interviewDetails.map((interviewMessage: Interview) => ({
+      ...interviewMessage,
+      interviewId
+    }))
+
+    await supabase.from('interview_details').insert(interviewDetailsData)
+
+    return NextResponse.json({
+      ok: true
+    })
+  } catch (error) {
+    console.error(error)
+    return NextResponse.json(
+      {
+        ok: false
+      },
+      {
+        status: 500
+      }
+    )
+  }
+}
