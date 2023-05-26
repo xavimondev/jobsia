2475984c19ad929afd3193e9754973b4975b9a45
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-25 22:18:09 -0500

    update page's name

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <section className='p-6 w-full h-screen grid place-items-center'>
+      <h2>Welcome to dashboard</h2>
+    </section>
+  )
+}
--- src/app/results/page.tsx
+++
@@ -1,9 +0,0 @@
-import { FeedbackInterview } from '@/components/feedback-interview'
-
-export default function Results() {
-  return (
-    <section className='p-6 w-full h-screen grid place-items-center'>
-      <FeedbackInterview />
-    </section>
-  )
-}
