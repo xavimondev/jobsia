03b9cc02792223b1e18481c0c1c25549842fc972
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-25 22:35:04 -0500

    update ui with small improvements

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -9,15 +9,10 @@ export default function Onboarding() {
       </section>
       <section className='hidden lg:flex flex-row items-center mx-auto max-w-7xl'>
         <div className='w-full'>
-          <h1 className='font-bold text-5xl text-blue-300 mb-5'>Bienvenido a InterviewIA</h1>
+          <h1 className='font-bold text-5xl text-blue-300 mb-5'>Bienvenido a Interview.ia</h1>
           <p className='text-white text-lg'>
-            InterviewIA es una plataforma que te ayuda a prepararte para tus próximas entrevistas de
-            trabajo.
-          </p>
-          <p className='text-white text-lg mt-3'>
-            Tendrás dos fases de entrevista, la primera consistirá en una{' '}
-            <span className='text-yellow-300 font-bold'>Entrevista inicial</span> y la segunda una{' '}
-            <span className='text-yellow-300 font-bold'>Entrevista técnica</span>.
+            Interview.ia es una plataforma que te ayuda a prepararte para tus próximas entrevistas
+            de trabajo.
           </p>
           <div className='mt-8'>
             <h2 className='font-bold text-2xl text-blue-300 mb-5'>
@@ -33,21 +28,7 @@ export default function Onboarding() {
               la siguiente fase.
             </p>
           </div>
-          <div className='mt-8'>
-            <h2 className='font-bold text-2xl text-blue-300 mb-5'>
-              ¿En que consiste la entrevista técnica?
-            </h2>
-            <p className='text-white text-lg'>
-              En este fase el asistente evaluará tus conocimientos técnicos solicitados en la oferta
-              laboral.
-            </p>
-            <p className='text-white text-lg'>
-              Al igual que la fase previa, debes obtener el{' '}
-              <span className='text-yellow-300 font-bold'>puntaje</span> necesario para alcanzar la
-              fase final.
-            </p>
-          </div>
-          <div className='p-3 border rounded-lg text-blue-400 border-blue-300 mt-4'>
+          <div className='p-3 border rounded-lg text-blue-400 border-blue-300 mt-6'>
             <div className='flex items-center justify-between'>
               <span className='font-semibold'>
                 Para empezar la prueba técnica necesitas autenticarte
