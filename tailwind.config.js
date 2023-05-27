8cb05433895ff40dda8a0455a3b4d5a907a3847a
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-27 11:11:00 -0500

    add tremor dependency

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = withAnimations({
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@tremor/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
