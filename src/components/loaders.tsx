616865eb080466a1743c5dd625fbd8792b89ae41
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-26 20:27:59 -0500

    add loader

--- src/components/loaders.tsx
+++ src/components/loaders.tsx
@@ -1,3 +1,4 @@
+import { LineWobble } from '@uiball/loaders'
 import { LoadingIc } from './icons'
 
 type RingLoaderProps = {
@@ -9,6 +10,20 @@ export function RingLoader({ msg }: RingLoaderProps) {
     <div className='flex flex-col gap-2 justify-center items-center mt-20 w-full'>
       <LoadingIc className='h-10 w-10' />
       <span className='font-bold text-white'>{msg}</span>
+    </div>
+  )
+}
+
+type LineWobbleLoader = {
+  msg: string
+  className: string
+}
+
+export function LineWobbleLoader({ msg, className }: LineWobbleLoader) {
+  return (
+    <div className='flex flex-col items-center gap-1'>
+      <LineWobble color='#FDE047' size={80} speed={3} lineWeight={6} />
+      <span className={className}>{msg}</span>
     </div>
   )
 }
