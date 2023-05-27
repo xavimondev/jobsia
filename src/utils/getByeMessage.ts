eb44c356388e77f0c5831863114299b66f356b78
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-26 20:13:49 -0500

    update farewell message

--- src/utils/getByeMessage.ts
+++ src/utils/getByeMessage.ts
@@ -1,6 +1,4 @@
 export const getByeMessage = (position: string, company: string, candidate: string) => {
   return `${candidate} quiero agradecerte sinceramente por dedicar tu tiempo al proceso de entrevista para el puesto de ${position} en ${company}.
-  Tu participación fue valiosa y apreciada.
-  Tu entusiasmo y habilidades son impresionantes, y estoy seguro de que encontrarás una gran oportunidad para crecer. 
-  A continuación verás los resultados de la entrevista`
+  Tu participación fue valiosa. A continuación verás los resultados de la entrevista.`
 }
