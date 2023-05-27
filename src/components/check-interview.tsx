68482de0921963f07977db64e869887f4a757c40
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-26 20:15:33 -0500

    update text color

--- src/components/check-interview.tsx
+++ src/components/check-interview.tsx
@@ -14,11 +14,11 @@ export function CheckInterview({ position, company, interviewDate }: CheckInterv
     <section className='h-screen grid place-items-center w-full mx-auto max-w-4xl'>
       <NotInterviewAllowed>
         <Blocked className='h-[200px] w-[200px] sm:w-[500px] sm:h-[500px]' />
-        <p className='font-semibold text-white text-3xl'>
+        <p className='font-semibold text-white text-xl xl:text-3xl'>
           Ya has realizado una entrevista para el puesto{' '}
-          <span className='text-blue-400'>{position}</span> en la empresa{' '}
-          <span className='text-blue-400'>{company}</span> el{' '}
-          <span className='text-blue-400'>{getReadableDate(interviewDate)}</span>
+          <span className='text-yellow-400'>{position}</span> en la empresa{' '}
+          <span className='text-yellow-400'>{company}</span> el{' '}
+          <span className='text-yellow-400'>{getReadableDate(interviewDate)}</span>
         </p>
         <div className='mt-6 flex items-center gap-4'>
           <Link
