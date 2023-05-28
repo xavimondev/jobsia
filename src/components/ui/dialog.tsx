d29e1c3079b160f9d5d2a09e86f1ec76a686b65b
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-28 11:23:00 -0500

    update ui styles

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -19,9 +19,9 @@ export const DialogUi = forwardRef<HTMLDivElement, PropsWithChildren<DialogUiPro
           <Dialog.Content
             {...props}
             ref={forwardRef}
-            className='data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-[#1d1b25] p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none'
+            className='data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[95vw] max-w-[850px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-[#1d1b25] p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none'
           >
-            <Dialog.Title className='text-gray-200 m-0 text-lg font-medium'>{title}</Dialog.Title>
+            <Dialog.Title className='text-white m-0 text-lg font-medium'>{title}</Dialog.Title>
             {description && (
               <Dialog.Description className='text-gray-400 mt-[10px] mb-5 text-[15px] leading-normal'>
                 {description}
