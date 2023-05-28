ed7da9706ad90a814d63b077caa56d76e396711c
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-27 22:11:20 -0500

    add icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -181,3 +181,11 @@ export function EyeIc(props: SVGProps<SVGSVGElement>) {
     </svg>
   )
 }
+
+export function NotIc(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' fill='currentColor' viewBox='0 0 256 256' {...props}>
+      <path d='M208,32H48A16,16,0,0,0,32,48V208a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V48A16,16,0,0,0,208,32ZM181.66,170.34a8,8,0,0,1-11.32,11.32L128,139.31,85.66,181.66a8,8,0,0,1-11.32-11.32L116.69,128,74.34,85.66A8,8,0,0,1,85.66,74.34L128,116.69l42.34-42.35a8,8,0,0,1,11.32,11.32L139.31,128Z'></path>
+    </svg>
+  )
+}
