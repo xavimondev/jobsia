c2c08d33c71c1cfc69ad9284c4ad9a81dfe84fba
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-27 22:07:17 -0500

    add indicators are displayed dynamically

--- src/components/dashboard/body-dashboard.tsx
+++ src/components/dashboard/body-dashboard.tsx
@@ -7,15 +7,18 @@ import { ListIndicators } from '@/components/dashboard/list-indicators'
 export function BodyDashboard() {
   const jobOfferIdSelected = useStore((state) => state.jobOfferIdSelected)
   const setInterviewReport = useStore((state) => state.setInterviewReport)
+  const setIndicators = useStore((state) => state.setIndicators)
 
   useEffect(() => {
     if (jobOfferIdSelected) {
       fetch(`/api/dashboard?offerid=${jobOfferIdSelected}`)
         .then((res) => res.json())
         .then((data) => {
-          if (data.interviews) {
-            //console.log(data.interviews)
-            setInterviewReport(data.interviews)
+          if (!data.error) {
+            // console.log(data)
+            const { indicators, interviews } = data
+            setIndicators(indicators)
+            setInterviewReport(interviews)
           }
         })
     }
--- src/components/dashboard/list-indicators.tsx
+++ src/components/dashboard/list-indicators.tsx
@@ -1,6 +1,10 @@
+import { useStore } from '@/store'
 import { UsersIc } from '@/components/icons'
 
 export function ListIndicators() {
+  const indicators = useStore((state) => state.indicators)
+  const { postulantes, seleccionados, noSeleccionados } = indicators
+
   return (
     <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-3 text-white mt-6'>
       <div className='rounded-lg border border-gray-400 border-opacity-25 shadow-sm'>
@@ -9,25 +13,25 @@ export function ListIndicators() {
           <UsersIc className='w-8 h-8 text-white' />
         </div>
         <div className='p-6 pt-0'>
-          <div className='text-2xl font-bold'>15</div>
+          <div className='text-2xl font-bold'>{postulantes}</div>
         </div>
       </div>
       <div className='rounded-lg border border-gray-400 border-opacity-25 shadow-sm'>
         <div className='p-6 flex flex-row items-center justify-between space-y-0 pb-3'>
-          <h3 className='text font-medium'>Postulantes</h3>
+          <h3 className='text font-medium'>Seleccionados</h3>
           <UsersIc className='w-8 h-8 text-white' />
         </div>
         <div className='p-6 pt-0'>
-          <div className='text-2xl font-bold'>15</div>
+          <div className='text-2xl font-bold'>{seleccionados}</div>
         </div>
       </div>
       <div className='rounded-lg border border-gray-400 border-opacity-25 shadow-sm'>
         <div className='p-6 flex flex-row items-center justify-between space-y-0 pb-3'>
-          <h3 className='text font-medium'>Postulantes</h3>
+          <h3 className='text font-medium'>No Seleccionados</h3>
           <UsersIc className='w-8 h-8 text-white' />
         </div>
         <div className='p-6 pt-0'>
-          <div className='text-2xl font-bold'>15</div>
+          <div className='text-2xl font-bold'>{noSeleccionados}</div>
         </div>
       </div>
     </div>
