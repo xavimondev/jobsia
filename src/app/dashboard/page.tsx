8a37582a6d3e98185794ed1a7d9b2295f90101ea
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-27 21:30:54 -0500

    refactor page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,6 @@
 import { getOffersCompany } from '@/services/position'
-import { ListOffersInterview } from '@/components/dashboard/list-offers-interview'
-import { ListInterviews } from '@/components/dashboard/list-interviews'
-import { ListIndicators } from '@/components/dashboard/list-indicators'
+import { HeaderDashboard } from '@/components/dashboard/header-dashboard'
+import { BodyDashboard } from '@/components/dashboard/body-dashboard'
 
 export default async function Dashboard() {
   const data = await getOffersCompany()
@@ -9,16 +8,8 @@ export default async function Dashboard() {
   return (
     <section className='p-6 w-full h-screen'>
       <div className='flex-1 p-6'>
-        <div className='flex items-center justify-between text-white'>
-          <h2 className='text-3xl font-bold tracking-tight'>Dashboard</h2>
-          <div className='flex items-center space-x-2'>
-            <div className='grid gap-2'>
-              <ListOffersInterview offers={data} />
-            </div>
-          </div>
-        </div>
-        <ListIndicators />
-        <ListInterviews />
+        <HeaderDashboard data={data} />
+        <BodyDashboard />
       </div>
     </section>
   )
