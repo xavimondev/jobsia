5665612644da212fa71639065209bcedb7dff674
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-29 15:44:26 -0500

    fix interviews were not displaying in select box

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,9 +2,9 @@ import { getOffersCompany } from '@/services/position'
 import { HeaderDashboard } from '@/components/dashboard/header-dashboard'
 import { BodyDashboard } from '@/components/dashboard/body-dashboard'
 
+export const dynamic = 'force-dynamic'
 export default async function Dashboard() {
   const data = await getOffersCompany()
-
   return (
     <section className='p-6 w-full h-screen'>
       <div className='flex-1 p-6'>
