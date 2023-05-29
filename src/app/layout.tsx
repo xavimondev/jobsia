9735d0edf8e9bfdf9605bbeffbcace5744de19b0
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-28 23:38:06 -0500

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react'
 import { AUTH_REDIRECT } from '@/utils/constants'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -26,7 +27,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <>
+          {children}
+          <Analytics />
+        </>
+      </body>
     </html>
   )
 }
