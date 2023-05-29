50f4f1db9a2adb64bc4cf3e1f121f12492030c5a
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-29 17:18:03 -0500

    update auth scopes

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -34,7 +34,7 @@ export default function Onboarding() {
                 Para empezar la prueba técnica necesitas autenticarte
               </span>
               <Link
-                href={`https://www.infojobs.net/api/oauth/user-authorize/index.xhtml?scope=MY_APPLICATIONS,CANDIDATE_PROFILE_WITH_EMAIL,CANDIDATE_READ_CURRICULUM_SKILLS,CV,CANDIDATE_READ_CURRICULUM_CVTEXT,CANDIDATE_READ_CURRICULUM_EDUCATION,CANDIDATE_READ_CURRICULUM_EXPERIENCE,COVER_LETTER_READ&client_id=${process.env.INFOJOBS_CLIENT_ID}&redirect_uri=${process.env.INFOJOBS_REDIRECT_URI}&response_type=code`}
+                href={`https://www.infojobs.net/api/oauth/user-authorize/index.xhtml?scope=MY_APPLICATIONS,CANDIDATE_PROFILE_WITH_EMAIL&client_id=${process.env.INFOJOBS_CLIENT_ID}&redirect_uri=${process.env.INFOJOBS_REDIRECT_URI}&response_type=code`}
                 className='rounded-md text-sm px-3 py-2 text-center bg-primary border hover:border-opacity-30 border-white border-opacity-10 text-[#bac8de] hover:text-white'
               >
                 Iniciar Sesión
