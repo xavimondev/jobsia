3e84c1894accefbe927647768e510cb47bcacadc
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-28 22:31:02 -0500

    add icons

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -205,3 +205,19 @@ export function SparkleIc(props: SVGProps<SVGSVGElement>) {
     </svg>
   )
 }
+
+export function HomeIc(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' fill='currentColor' viewBox='0 0 256 256' {...props}>
+      <path d='M224,115.55V208a16,16,0,0,1-16,16H168a16,16,0,0,1-16-16V168a8,8,0,0,0-8-8H112a8,8,0,0,0-8,8v40a16,16,0,0,1-16,16H48a16,16,0,0,1-16-16V115.55a16,16,0,0,1,5.17-11.78l80-75.48.11-.11a16,16,0,0,1,21.53,0,1.14,1.14,0,0,0,.11.11l80,75.48A16,16,0,0,1,224,115.55Z'></path>
+    </svg>
+  )
+}
+
+export function CaretRight(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' fill='currentColor' viewBox='0 0 256 256' {...props}>
+      <path d='M184.49,136.49l-80,80a12,12,0,0,1-17-17L159,128,87.51,56.49a12,12,0,1,1,17-17l80,80A12,12,0,0,1,184.49,136.49Z'></path>
+    </svg>
+  )
+}
