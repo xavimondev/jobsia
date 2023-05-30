ffafc5c2f3550bf63eb73dc83ae48c7ec9c5b3ba
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-29 22:38:36 -0500

    add util to resume job offer

---
+++ src/utils/getResumeOffer.ts
@@ -0,0 +1,30 @@
+import { formatString } from './formatString'
+
+export const getResumeOffer = (
+  salaryDescription: string,
+  experienceMin: string,
+  study: string,
+  contractType: string,
+  requirementMin: string
+) => {
+  const requirements = `${
+    salaryDescription === 'Salario no disponible'
+      ? ''
+      : `Con un salario de ${salaryDescription.replace('-', 'a')}.`
+  }${
+    experienceMin === 'No Requerida'
+      ? 'No se requiere experiencia'
+      : `Se requiere experiencia de ${experienceMin.toLowerCase()}`
+  } y ${
+    study === 'Sin estudios' ? 'no se requiere estudios' : `con estudios de ${study}`
+  }.El contrato es ${contractType.toLowerCase()}.${
+    requirementMin === ''
+      ? 'No se especifican los requerimientos mínimos para esta oferta laboral'
+      : `El trabajo tiene los siguientes requerimientos:${requirementMin
+          .replace(/\n/g, '')
+          .replace(/-/g, '')
+          .replace('·', '')}`
+  }`
+
+  return formatString(requirements)
+}
