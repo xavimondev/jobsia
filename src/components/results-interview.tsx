e96682d524af02470a7a42c03097e0786f58aa42
Xavier Alfaro <nxb.developer@gmail.com>
2023-05-30 23:43:09 -0500

    add emoji confetti

--- src/components/results-interview.tsx
+++ src/components/results-interview.tsx
@@ -1,3 +1,6 @@
+'use client'
+import { useEffect } from 'react'
+import JSConfetti from 'js-confetti'
 import { SCORE_TO_ACCOMPLISH } from '@/utils/constants'
 import { useStore } from '@/store'
 import { Discarded, EvaluationExcellent } from './illustrations'
@@ -7,6 +10,22 @@ import { ScoreInterview } from './score-interview'
 export function ResultsInterview() {
   const getTotalScore = useStore((state) => state.getTotalScore)
   const hasIdealScore = getTotalScore() >= SCORE_TO_ACCOMPLISH
+
+  useEffect(() => {
+    const jsConfetti = new JSConfetti()
+    const emojis = hasIdealScore ? ['👏', '🎉', '🥳'] : ['😢', '❌', '😞']
+
+    jsConfetti.addConfetti({
+      emojis,
+      emojiSize: 40,
+      confettiNumber: 60,
+      confettiRadius: 5
+    })
+
+    return () => {
+      jsConfetti.clearCanvas()
+    }
+  }, [])
 
   return (
     <section className='p-6 w-full h-screen grid place-items-center animate-fadeIn'>
